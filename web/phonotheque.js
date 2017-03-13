5b730ba16c961b39f1852523117b7ff86b47f33c
aricci <aricci@noemalife.com>
2017-03-13 17:57:23 +0100

    Minor changes in phonotheque.js

--- web/phonotheque.js
+++ web/phonotheque.js
@@ -17,12 +17,12 @@ server.connection({
 });
 
 server.route({path:'/', method:'GET', handler:{file:'index.html'}});
-server.route({path:'/doc', method:'GET', config:{handler:getDocument}});
-server.route({path:'/doc/{id}', method:'GET', config:{handler:getDocument}});
-/**server.route({path:'/doc/review', method:'GET', config:{handler:getReview}});
-server.route({path:'/doc/review/{id}', method:'GET', config:{handler:getReview}});
-server.route({path:'/doc/monograph', method:'GET', config:{handler:getMonograph}});
-server.route({path:'/doc/monograph/{id}', method:'GET', config:{handler:getMonograph}});*/
+server.route({path:'/api/doc', method:'GET', config:{handler:getDocument}});
+server.route({path:'/api/doc/{id}', method:'GET', config:{handler:getDocument}});
+server.route({path:'/api/doc/review', method:'GET', config:{handler:getReview}});
+server.route({path:'/api/doc/review/{id}', method:'GET', config:{handler:getReview}});
+server.route({path:'/api/doc/monograph', method:'GET', config:{handler:getMonograph}});
+server.route({path:'/api/doc/monograph/{id}', method:'GET', config:{handler:getMonograph}});
 
 server.route({path:'/{file}', method:'GET', handler:{directory:{path:'./'}}});
 server.route({path:'/css/{file}', method:'GET', handler:{directory:{path:'./css'}}});
@@ -32,14 +32,14 @@ server.route({path:'/images/{file}', method:'GET', handler:{directory:{path:'./i
 
 server.start((err) => {
     if (err) {throw err;}
-    console.log('Server running at:', server.info.uri);
+    console.log('Server: Listening on ' + server.info.uri);
 });
 
 function getDocument(request, reply)
 {
 	if(request.params.id)
 	{
-		console.log('Finding document with id ' + request.params.id);
+		console.log('Document: document with id ' + request.params.id);
 		articles.find({'id': request.params.id}).toArray(function(err, docs) {
 			reply(docs);
 		});
@@ -48,6 +48,42 @@ function getDocument(request, reply)
 	{
 		var page = Number(request.query.page) > 0 ? Number(request.query.page) : 0;
 		articles.find().skip(page*10).limit(10).sort({"creationDate":-1}).toArray(function(err, docs) {
+			reply(docs);
+		});
+	}
+}
+
+function getReview(request, reply)
+{
+	if(request.params.id)
+	{
+		console.log('Review: document with id ' + request.params.id);
+		articles.find({'id': request.params.id}).toArray(function(err, docs) {
+			reply(docs);
+		});
+	}
+	else
+	{
+		var page = Number(request.query.page) > 0 ? Number(request.query.page) : 0;
+		articles.find({'type': 'REVIEW'}).skip(page*10).limit(10).sort({"creationDate":-1}).toArray(function(err, docs) {
+			reply(docs);
+		});
+	}
+}
+
+function getMonograph(request, reply)
+{
+	if(request.params.id)
+	{
+		console.log('Monograph: document with id ' + request.params.id);
+		articles.find({'id': request.params.id}).toArray(function(err, docs) {
+			reply(docs);
+		});
+	}
+	else
+	{
+		var page = Number(request.query.page) > 0 ? Number(request.query.page) : 0;
+		articles.find({'type': 'MONOGRAPH'}).skip(page*10).limit(10).sort({"creationDate":-1}).toArray(function(err, docs) {
 			reply(docs);
 		});
 	}

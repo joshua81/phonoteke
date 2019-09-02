b330b0482ee5e04d2280397fc207002e2d390ab9
Andrea Ricci <magoarix@gmail.com>
2019-09-02 15:27:38 +0200

    General refactoring

--- src/main/java/org/phonoteke/crawler/OndarockCrawler.java
+++ src/main/java/org/phonoteke/crawler/OndarockCrawler.java
@@ -1,85 +1,28 @@
 package org.phonoteke.crawler;
 
-import org.bson.Document;
-
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.model.Filters;
-
-import edu.uci.ics.crawler4j.crawler.CrawlConfig;
-import edu.uci.ics.crawler4j.crawler.CrawlController;
-import edu.uci.ics.crawler4j.crawler.Page;
-import edu.uci.ics.crawler4j.fetcher.PageFetcher;
-import edu.uci.ics.crawler4j.parser.HtmlParseData;
-import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
-import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
-import edu.uci.ics.crawler4j.url.WebURL;
-
 public class OndarockCrawler extends PhonotekeCrawler
 {
-	public static final String URL = "https://www.ondarock.it/";
-	
-//	private static final Pattern FILTERS = Pattern.compile(".*(\\.(htm|html))$");
+	private static final String URL = "https://www.ondarock.it/";
+	private static final String SOURCE = "ondarock";
 
-	private MongoCollection<Document> pages;
-	
 
 	public OndarockCrawler()
 	{
 		super();
 	}
 	
-	public void crawl()
+	protected String getBaseUrl()
 	{
-		try
-		{
-			CrawlConfig config = new CrawlConfig();
-			config.setCrawlStorageFolder(CRAWL_STORAGE_FOLDER);
-			PageFetcher pageFetcher = new PageFetcher(config);
-			RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
-			RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
-			
-			CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
-			controller.addSeed(URL);
-			controller.start(OndarockCrawler.class, NUMBER_OF_CRAWLERS);
-		} 
-		catch (Throwable t) 
-		{
-			LOGGER.error("Error crawling Musilcabox: " + t.getMessage());
-			throw new RuntimeException(t);
-		}
+		return URL;
 	}
-
-	@Override
-	public boolean shouldVisit(Page referringPage, WebURL url) {
-		String dest = url.getURL().toLowerCase();
-		return dest.startsWith(URL);
+	
+	protected Boolean shouldVisit(String url) 
+	{
+		return url.startsWith(URL);
 	}
-
-	@Override
-	public void visit(Page page) {
-		if (page.getParseData() instanceof HtmlParseData) 
-		{
-			HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
-			String html = htmlParseData.getHtml();
-			String url = page.getWebURL().getURL();
-
-			try
-			{
-				Document json = pages.find(Filters.and(Filters.eq("url", url))).first();
-				if(json == null)
-				{
-					json = new Document("url", url).
-							append("page", html).
-							append("source", "ondarock");
-					pages.insertOne(json);
-					LOGGER.info("Page " + url + " added");
-				}
-			} 
-			catch (Throwable t) 
-			{
-				LOGGER.error("Error parsing page " + url + ": " + t.getMessage());
-				throw new RuntimeException(t);
-			}
-		}
+	
+	protected String getSource() 
+	{
+		return SOURCE;
 	}
 }
--- src/main/java/org/phonoteke/loader/PhonotekeLoader.java
+++ src/main/java/org/phonoteke/loader/PhonotekeLoader.java
@@ -1,11 +1,22 @@
 package org.phonoteke.loader;
 
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.phonoteke.model.Track;
 
+import com.google.common.hash.Hashing;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 
 public class PhonotekeLoader 
 {
@@ -18,13 +29,19 @@ public class PhonotekeLoader
 	protected MongoCollection<org.bson.Document> pages;
 	protected MongoCollection<org.bson.Document> albums;
 	protected MongoCollection<org.bson.Document> artists;
-	
+
+	protected enum TYPE {
+		ARTIST,
+		ALBUM,
+		UNKNOWN
+	}
+
 	public static void main(String[] args) 
 	{
 		new OndarockLoader().load();
 		new MusicalboxLoader().load();
 	}
-	
+
 	public PhonotekeLoader()
 	{
 		try
@@ -39,5 +56,212 @@ public class PhonotekeLoader
 			LOGGER.error("Error connecting to Mongo db: " + t.getMessage());
 			throw new RuntimeException(t);
 		}
+	}
+
+	protected void load()
+	{
+		//		http://musicbrainz.org/ws/2/recording/?query=artist:BROOKZILL%20AND%20recording:LET%E2%80%99S%20GO%20(E%20NOIZ)!
+		//		MongoCursor<org.bson.Document> i = pages.find(Filters.eq("url", "https://www.raiplayradio.it/audio/2017/05/2Night-Musicalbox-del-18052017-958ca610-d269-45e7-a665-b60f21a18590.html")).iterator();
+		String source = getSource();
+		MongoCursor<org.bson.Document> i = pages.find(Filters.eq("source", source)).iterator();
+		while(i.hasNext())
+		{
+			org.bson.Document page = i.next();
+			String url = page.get("url", String.class);
+			String html = page.get("page", String.class);
+
+			TYPE type = getType(url);
+			if(TYPE.ALBUM.equals(type))
+			{
+				// check if the article was already crawled
+				MongoCursor<org.bson.Document> j = albums.find(Filters.and(
+						Filters.eq("source", source), 
+						Filters.eq("url", url))).iterator();
+				if(!j.hasNext())
+				{
+					try
+					{
+						Document doc = Jsoup.parse(html);
+						org.bson.Document json = new org.bson.Document("id", getId(url)).
+								append("artist", getArtist(url, doc)).
+								append("authors", getAuthors(url, doc)).
+								append("cover", getCover(url, doc)).
+								append("date", getDate(url, doc)).
+								append("description", getDescription(url, doc)).
+								append("genres", getGenres(url, doc)).
+								append("idmbrz", getIdmbrz(url, doc)).
+								append("idsptf", getIdsptf(url, doc)).
+								append("label", getLabel(url, doc)).
+								append("milestone", getMilestone(url, doc)).
+								append("review", getReview(url, doc)).
+								append("source", getSource()).
+								append("title", getTitle(url, doc)).
+								append("tracks", getTracks(url, doc)).
+								append("url", getUrl(url, doc)).
+								append("vote", getVote(url, doc)).
+								append("year", getYear(url, doc));
+						albums.insertOne(json);
+						LOGGER.info("Album " + url + " added");
+					}
+					catch (Throwable t) 
+					{
+						LOGGER.error("Error parsing page " + url + ": " + t.getMessage());
+					}
+				}
+			}
+			else if(TYPE.ARTIST.equals(type))
+			{
+				// check if the article was already crawled
+				MongoCursor<org.bson.Document> j = artists.find(Filters.and(
+						Filters.eq("source", source), 
+						Filters.eq("url", url))).iterator();
+				if(!j.hasNext())
+				{
+					try
+					{
+						Document doc = Jsoup.parse(html);
+						org.bson.Document json = new org.bson.Document("id", getId(url)).
+								append("authors", getAuthors(url, doc)).
+								append("cover", getCover(url, doc)).
+								append("date", getDate(url, doc)).
+								append("description", getDescription(url, doc)).
+								append("idmbrz", getIdmbrz(url, doc)).
+								append("idsptf", getIdsptf(url, doc)).
+								append("name", getName(url, doc)).
+								append("review", getReview(url, doc)).
+								append("source", getSource()).
+								append("url", getUrl(url, doc));
+						artists.insertOne(json);
+						LOGGER.info("Artist " + url + " added");
+					}
+					catch (Throwable t) 
+					{
+						LOGGER.error("Error parsing page " + url + ": " + t.getMessage());
+					}
+				}
+			}
+		}
+	}
+
+	//	private void delete()
+	//	{
+	//		MongoCursor<org.bson.Document> i = albums.find(Filters.eq("content", "")).iterator();
+	//		while(i.hasNext())
+	//		{
+	//			org.bson.Document page = i.next();
+	//			String url = page.get("url", String.class);
+	//			albums.findOneAndDelete(Filters.eq("url", url));
+	//			pages.findOneAndDelete(Filters.eq("url", url));
+	//			LOGGER.info("Deleted page " + url);
+	//		}
+	//	}
+
+	protected String getId(String url) 
+	{
+		return Hashing.sha256().hashString(getUrl(url), StandardCharsets.UTF_8).toString();
+	}
+
+	protected String getUrl(String url) 
+	{
+		try 
+		{
+			if(url.startsWith(".") || url.startsWith("/"))
+			{
+				url = new URL(new URL(getBaseUrl()), url).toString();
+				url = url.replaceAll("\\.\\./", "");
+			}
+			return url.trim();
+		} 
+		catch (Throwable t) 
+		{
+			LOGGER.error("Error getUrl() "+ url + ": " + t.getMessage());
+			return null;
+		} 
+	}
+
+	//---------------------------------
+	// Methods to be overridden
+	//---------------------------------
+	protected String getBaseUrl()
+	{
+		return null;
+	}
+
+	protected String getSource() 
+	{
+		return null;
+	}
+
+	protected TYPE getType(String url) 
+	{
+		return TYPE.UNKNOWN;
+	}
+
+	protected String getArtist(String url, Document doc) {
+		return null;
+	}
+
+	protected String getName(String url, Document doc) {
+		return null;
+	}
+
+	protected List<String> getAuthors(String url, Document doc) {
+		return null;
+	}
+
+	protected String getCover(String url, Document doc) {
+		return null;
+	}
+
+	protected Date getDate(String url, Document doc) {
+		return null;
+	}
+
+	protected String getDescription(String url, Document doc) {
+		return null;
+	}
+
+	protected List<String> getGenres(String url, Document doc) {
+		return null;
+	}
+
+	protected String getIdmbrz(String url, Document doc) {
+		return null;
+	}
+
+	protected String getIdsptf(String url, Document doc) {
+		return null;
+	}
+
+	protected String getLabel(String url, Document doc) {
+		return null;
+	}
+
+	protected Boolean getMilestone(String url, Document doc) {
+		return null;
+	}
+
+	protected String getReview(String url, Document doc) {
+		return null;
+	}
+
+	protected String getTitle(String url, Document doc) {
+		return null;
+	}
+
+	protected List<Track> getTracks(String url, Document doc) {
+		return null;
+	}
+
+	protected String getUrl(String url, Document doc) {
+		return null;
+	}
+
+	protected Float getVote(String url, Document doc) {
+		return null;
+	}
+
+	protected Integer getYear(String url, Document doc) {
+		return null;
 	}
 }

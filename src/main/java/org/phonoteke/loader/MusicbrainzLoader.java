09c2cd11c416f2ea4371ab44ab4b9d16ff49fd34
Andrea Ricci <magoarix@gmail.com>
2019-09-18 19:50:49 +0200

    Added MusicbrainzLoader

--- src/main/java/org/phonoteke/loader/MusicbrainzLoader.java
+++ src/main/java/org/phonoteke/loader/MusicbrainzLoader.java
@@ -4,13 +4,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
@@ -28,12 +27,15 @@ public class MusicbrainzLoader
 	private static final String MUSICBRAINZ = "http://musicbrainz.org/ws/2";
 
 	private MongoCollection<org.bson.Document> docs;
+	private MongoCollection<org.bson.Document> tracks;
 	private MongoCollection<org.bson.Document> musicbrainz;
 
 
 	public static void main(String[] args) 
 	{
-		new MusicbrainzLoader().loadAlbum();
+		//		new MusicbrainzLoader().loadAlbums();
+		//		new MusicbrainzLoader().loadArtists();
+		new MusicbrainzLoader().loadTracks();
 	}
 
 	public MusicbrainzLoader()
@@ -42,6 +44,7 @@ public class MusicbrainzLoader
 		{
 			MongoDatabase db = new MongoClient(MONGO_HOST, MONGO_PORT).getDatabase(MONGO_DB);
 			docs = db.getCollection("docs");
+			tracks = db.getCollection("tracks");
 			musicbrainz = db.getCollection("musicbrainz");
 		} 
 		catch (Throwable t) 
@@ -51,7 +54,7 @@ public class MusicbrainzLoader
 		}
 	}
 
-	private void loadAlbum()
+	private void loadAlbums()
 	{
 		MongoCursor<org.bson.Document> i = docs.find(Filters.and(Filters.eq("type", "album"))).iterator();
 		while(i.hasNext())
@@ -67,7 +70,7 @@ public class MusicbrainzLoader
 				{
 					json.append("id", id);
 					musicbrainz.insertOne(json);
-					LOGGER.info("Musicbrainz " + id + " added");
+					LOGGER.info("Musicbrainz " + id + " Album added");
 				}
 				try {
 					Thread.sleep(3000);
@@ -78,55 +81,123 @@ public class MusicbrainzLoader
 		}
 	}
 
-	private String getArtistd(String artist)
+	private void loadArtists()
 	{
-		BufferedReader rd = null;
-		try
+		MongoCursor<org.bson.Document> i = docs.find(Filters.and(Filters.eq("type", "artist"))).iterator();
+		while(i.hasNext())
 		{
-			String urlString = MUSICBRAINZ + "/artist/?query=artist:" + artist.trim().replace(" ", "%20") + "&fmt=json";
-			String result = "";
-			String line = null;
-			HttpURLConnection con = (HttpURLConnection) new URL(urlString).openConnection();
-			rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			org.bson.Document page = i.next();
+			String id = page.get("id", String.class);
+			String artist = page.get("artist", String.class);
+			if(!musicbrainz.find(Filters.eq("id", id)).iterator().hasNext())
+			{
+				org.bson.Document json = getArtist(artist);
+				if(json != null)
+				{
+					json.append("id", id);
+					musicbrainz.insertOne(json);
+					LOGGER.info("Musicbrainz " + id + " Artist added");
+				}
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					// do nothing
+				}
+			}
+		}
+	}
+
+	private void loadTracks()
+	{
+		MongoCursor<org.bson.Document> i = tracks.find().iterator();
+		while(i.hasNext())
+		{
+			org.bson.Document page = i.next();
+			String id = page.get("id", String.class);
+			List<org.bson.Document> recordings = page.get("tracks", List.class);
+			for(org.bson.Document recording : recordings)
+			{
+				String title = recording.get("title", String.class);
+				org.bson.Document json = recording.get("musicbrainz", org.bson.Document.class);
+				if(json == null && StringUtils.isNoneBlank(title))
+				{
+					json = getRecording(title);
+					if(json != null)
+					{
+						recording.append("musicbrainz", json);
+					}
+					try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						// do nothing
+					}
+				}
+			}
+			tracks.updateOne(Filters.eq("id", id), new org.bson.Document("$set", page));
+			LOGGER.info("Musicbrainz " + id + " Tracks updated");
+		}
+	}
+
+	private org.bson.Document getRecording(String recording)
+	{
+		String urlString = MUSICBRAINZ + "/recording/?query=recording:" + recording.trim().replace(" ", "%20") + "&fmt=json";
+//		String urlString = MUSICBRAINZ + "/recording/?query=artist:" + artist.trim().replace(" ", "%20") + "%20AND%20" + "recording:" + album.trim().replace(" ", "%20") + "&fmt=json";
+		String json = "";
+		String line = null;
+		HttpURLConnection con;
+		try 
+		{
+			con = (HttpURLConnection)new URL(urlString).openConnection();
+		} 
+		catch(Throwable t)
+		{
+			LOGGER.error("ERROR: " + t.getMessage());
+			return null;
+		}
+
+		try(BufferedReader rd = new BufferedReader(new InputStreamReader(con.getInputStream()));)
+		{
 			while ((line = rd.readLine()) != null) 
 			{
-				result += line;
+				json += line;
 			}
-			BasicDBObject doc = BasicDBObject.parse(result);
-			doc.get("artist");
-
-			List<String> ids = new ArrayList<String>();
-			//			Elements artistElements = doc.select("artist");
-			//			for(int i = 0; i < artistElements.size(); i++)
-			//			{
-			//				Element artistElement = artistElements.get(i);
-			//				int score = new Integer(artistElement.attr("ext:score"));
-			//				if(score == 100)
-			//				{
-			//					ids.add(artistElement.attr("id"));
-			//				}
-			//			}
-
-			return ids.size() == 1 ? ids.get(0) : null;
+			return org.bson.Document.parse(json);
 		}
 		catch(Throwable t)
 		{
 			LOGGER.error("ERROR: " + t.getMessage());
 			return null;
 		}
-		finally
+	}
+
+	private org.bson.Document getArtist(String artist)
+	{
+		String urlString = MUSICBRAINZ + "/artist/?query=artist:" + artist.trim().replace(" ", "%20") + "&fmt=json";
+		String json = "";
+		String line = null;
+		HttpURLConnection con;
+		try 
 		{
-			if(rd != null)
+			con = (HttpURLConnection) new URL(urlString).openConnection();
+		} 
+		catch(Throwable t)
+		{
+			LOGGER.error("ERROR: " + t.getMessage());
+			return null;
+		}
+
+		try(BufferedReader rd = new BufferedReader(new InputStreamReader(con.getInputStream()));)
+		{
+			while ((line = rd.readLine()) != null) 
 			{
-				try 
-				{
-					rd.close();
-				} 
-				catch (Throwable t) 
-				{
-					// do nothing
-				}
+				json += line;
 			}
+			return org.bson.Document.parse(json);
+		}
+		catch(Throwable t)
+		{
+			LOGGER.error("ERROR: " + t.getMessage());
+			return null;
 		}
 	}
 
@@ -161,57 +232,57 @@ public class MusicbrainzLoader
 		}
 	}
 
-	private String[] getAlbumIds(String artist, String album)
-	{
-		BufferedReader rd = null;
-		try
-		{
-			String urlString = MUSICBRAINZ + "/release/?query=artist:" + artist.trim().replace(" ", "%20") + "%20AND%20" + "release:" + album.trim().replace(" ", "%20") + "&fmt=json";
-			String json = "";
-			String line = null;
-			HttpURLConnection con = (HttpURLConnection) new URL(urlString).openConnection();
-			rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			while ((line = rd.readLine()) != null) 
-			{
-				json += line;
-			}
-			BasicDBObject doc = BasicDBObject.parse(json);
-			List<BasicDBObject> releases = (List<BasicDBObject>)doc.get("releases");
-
-			for(BasicDBObject release : releases)
-			{
-				int score = (Integer)release.get("score");
-				if(score == 100)
-				{
-					for(BasicDBObject ac : (List<BasicDBObject>)release.get("artist-credit"))
-					{
-						String artistId = (String)((BasicDBObject)((List<BasicDBObject>)release.get("artist-credit")).get(0).get("artist")).get("id");
-						String albumId = (String)((BasicDBObject)release.get("release-group")).get("id");
-						//String amazonId = (String)release.get("asin");
-						return new String[]{artistId, albumId};
-					}
-				}
-			}
-			return null;
-		}
-		catch(Throwable t)
-		{
-			LOGGER.error("ERROR: " + t.getMessage());
-			return null;
-		}
-		finally
-		{
-			if(rd != null)
-			{
-				try 
-				{
-					rd.close();
-				} 
-				catch (Throwable t) 
-				{
-					// do nothing
-				}
-			}
-		}
-	}
+	//	private String[] getAlbumIds(String artist, String album)
+	//	{
+	//		BufferedReader rd = null;
+	//		try
+	//		{
+	//			String urlString = MUSICBRAINZ + "/release/?query=artist:" + artist.trim().replace(" ", "%20") + "%20AND%20" + "release:" + album.trim().replace(" ", "%20") + "&fmt=json";
+	//			String json = "";
+	//			String line = null;
+	//			HttpURLConnection con = (HttpURLConnection) new URL(urlString).openConnection();
+	//			rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
+	//			while ((line = rd.readLine()) != null) 
+	//			{
+	//				json += line;
+	//			}
+	//			BasicDBObject doc = BasicDBObject.parse(json);
+	//			List<BasicDBObject> releases = (List<BasicDBObject>)doc.get("releases");
+	//
+	//			for(BasicDBObject release : releases)
+	//			{
+	//				int score = (Integer)release.get("score");
+	//				if(score == 100)
+	//				{
+	//					for(BasicDBObject ac : (List<BasicDBObject>)release.get("artist-credit"))
+	//					{
+	//						String artistId = (String)((BasicDBObject)((List<BasicDBObject>)release.get("artist-credit")).get(0).get("artist")).get("id");
+	//						String albumId = (String)((BasicDBObject)release.get("release-group")).get("id");
+	//						//String amazonId = (String)release.get("asin");
+	//						return new String[]{artistId, albumId};
+	//					}
+	//				}
+	//			}
+	//			return null;
+	//		}
+	//		catch(Throwable t)
+	//		{
+	//			LOGGER.error("ERROR: " + t.getMessage());
+	//			return null;
+	//		}
+	//		finally
+	//		{
+	//			if(rd != null)
+	//			{
+	//				try 
+	//				{
+	//					rd.close();
+	//				} 
+	//				catch (Throwable t) 
+	//				{
+	//					// do nothing
+	//				}
+	//			}
+	//		}
+	//	}
 }

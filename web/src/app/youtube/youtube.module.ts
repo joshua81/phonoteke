8bd27b0d7aafb6451919897d75de2c76b1f20973
Andrea Ricci <andrea.ricci@prometeia.com>
2019-12-27 11:27:37 +0100

    Added video player

--- web/src/app/youtube/youtube.module.ts
+++ web/src/app/youtube/youtube.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { YouTubePlayerModule } from '@angular/youtube-player';
 import { YoutubeComponent } from './youtube.component';
 
 @NgModule({
-  imports: [YouTubePlayerModule],
+  imports: [YouTubePlayerModule, CommonModule],
   declarations: [YoutubeComponent],
   exports: [YoutubeComponent]
 })

eb2105f1bd14dd41f9990dfca49d423d539b0e20
Andrea Ricci <andrea.ricci@prometeia.com>
2020-03-13 09:04:13 +0100

    Fixed bug when loading Links

--- web/src/app/doc/doc.component.ts
+++ web/src/app/doc/doc.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { Meta } from '@angular/platform-browser'; 
 import { AppService } from '../app.service';
 
 @Component({
@@ -16,46 +15,26 @@ export class DocComponent implements OnInit {
   doc = null;
   links = [];
 
-  constructor(private http: HttpClient, private route: ActivatedRoute, private meta: Meta, public service: AppService) {}
+  constructor(private http: HttpClient, private route: ActivatedRoute, public service: AppService) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.type = params.get('type');
       this.id = params.get('id');
-      this.loadDoc(this.id);
-      this.loadLinks(this.id);
+      this.loadDoc();
+      this.loadLinks();
     });
   }
 
-  loadDoc(id: string) {
+  loadDoc() {
     this.service.resetEvents();
-    this.http.get('/api/' + this.type + '/' + id).subscribe(
+    this.http.get('/api/' + this.type + '/' + this.id).subscribe(
       (data: any) => this.setDoc(data[0]),
-      error => this.error = error);
-  }
-
-  loadLinks(id: string) {
-    this.links = [];
-    this.http.get('/api/' + this.type + '/' + id + '/links').subscribe(
-      (data: any) => this.setLinks(data),
       error => this.error = error);
   }
 
   setDoc(doc: any) {
     this.doc = doc;
-    this.meta.updateTag({ name: 'og:site_name', content: 'Human Beats' });
-    this.meta.updateTag({ name: 'og:title', content: this.doc.artist + ' - ' + this.doc.title + ' :: Human Beats' });
-    this.meta.updateTag({ name: 'og:type', content: 'music.album' });
-    //this.meta.updateTag({ name: 'og:type', content: 'music.playlist' });
-    this.meta.updateTag({ name: 'og:image', content: this.doc.cover });
-    this.meta.updateTag({ name: 'og:url', content: 'https://humanbeats.appspot.com/docs/' + this.doc.id });
-    this.meta.updateTag({ name: 'og:locale', content: 'it_IT' });
-    this.meta.updateTag({ name: 'og:description', content: this.doc.description });
-    this.meta.updateTag({ name: 'music:musician', content: this.doc.artist });
-    this.meta.updateTag({ name: 'music:release_date', content: this.doc.year + '-01-01' });
-    this.meta.updateTag({ name: 'music:album', content: this.doc.title });
-    //this.meta.updateTag({ name: 'music:creator', content: this.doc.title });
-
     if(this.service.audio){
       this.service.audio.pause();
       this.service.audio = null;
@@ -65,13 +44,13 @@ export class DocComponent implements OnInit {
       this.service.audio.src = this.doc.audio;
       this.service.audio.load();
     }
+  }
 
-    this.service.resetEvents();
-    this.links.splice(0, this.links.length);
-
-    if(this.doc.artistid) {
-      this.loadLinks(this.doc.artistid);
-    }
+  loadLinks() {
+    this.links = [];
+    this.http.get('/api/' + this.type + '/' + this.id + '/links').subscribe(
+      (data: any) => this.setLinks(data),
+      error => this.error = error);
   }
 
   setLinks(links: any) {

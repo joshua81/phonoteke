3624c471ba06131e000fa54cbb11d41e65ab372e
Andrea Ricci <andrea.ricci@prometeia.com>
2020-12-04 10:13:35 +0100

    Loaders minor changes

--- src/main/java/org/phonoteke/loader/HumanBeats.java
+++ src/main/java/org/phonoteke/loader/HumanBeats.java
@@ -241,7 +241,7 @@ public interface HumanBeats
 	public static String cleanText(String text) 
 	{
 		// strips off all non-ASCII characters
-		text = text.replaceAll("[^\\x00-\\x7F]", "");
+		//text = text.replaceAll("[^\\x00-\\x7F]", "");
 		// erases all the ASCII control characters
 		text = text.replaceAll("[\\p{Cntrl}&&[^\r\n\t]]", "");
 		// removes non-printable characters from Unicode

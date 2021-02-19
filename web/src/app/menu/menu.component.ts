9a4bfd284c6fdbd1033649e652db7c871ca62155
Andrea Ricci <andrea.ricci@prometeia.com>
2021-02-19 09:25:36 +0100

    FE router minor changes

--- web/src/app/menu/menu.component.ts
+++ web/src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AppComponent } from '../app.component';
 
@@ -8,15 +8,13 @@ import { AppComponent } from '../app.component';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-  type: string = null;
   source: string = null;
   searchText: string = '';
   showSearch: boolean = false;
   
   constructor(public app: AppComponent, private router: Router, private route: ActivatedRoute) {
     this.route.url.subscribe(params => {
-      this.type = params.length == 0 ? 'podcasts' : params[0].path;
-      this.source = params.length == 2 ? params[1].path : null;
+      this.source = params.length == 0 ? 'podcasts' : params[0].path;
     });
   }
 
@@ -24,9 +22,7 @@ export class MenuComponent implements OnInit {
 
   search() {
     if(this.searchText != null && this.searchText != '') {
-      this.router.navigate([this.type + (this.source == null ? '' : '/' + this.source)], {
-        queryParams: { q: this.searchText }
-      });
+      this.router.navigate([this.router.url], {queryParams: { q: this.searchText }});
     }
     this.toggleSearch();
   }

457528f615f2c4e9b29328d9305cc5bd8d2ed525
Andrea Ricci <andrea.ricci@prometeia.com>
2021-04-08 16:27:36 +0200

    Footer update

--- web/src/app/footer/footer.component.ts
+++ web/src/app/footer/footer.component.ts
@@ -1,12 +1,18 @@
 import {Component, OnInit} from '@angular/core';
+import { AppComponent } from '../app.component';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  showFooter: boolean = false;
 
-  constructor() {}
+  constructor(public app: AppComponent) {}
 
   ngOnInit() {}
+
+  toggleFooter() {
+    this.showFooter = !this.showFooter;
+  }
 }

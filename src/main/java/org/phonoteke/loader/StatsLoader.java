d879e09a9ff751fe3d59e184a1592721b0fcb340
Andrea Ricci <andrea.ricci@gmx.com>
2022-03-21 18:23:15 +0100

    Stats Loader review

--- src/main/java/org/phonoteke/loader/StatsLoader.java
+++ src/main/java/org/phonoteke/loader/StatsLoader.java
@@ -1,6 +1,7 @@
 package org.phonoteke.loader;
 
-import java.text.NumberFormat;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
 import java.util.List;
 import java.util.TreeMap;
 
@@ -9,6 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bson.Document;
 
+import com.google.api.client.util.Lists;
 import com.google.api.client.util.Maps;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.model.Filters;
@@ -16,7 +18,7 @@ import com.mongodb.client.model.Filters;
 public class StatsLoader extends HumanBeats
 {
 	private static final Logger LOGGER = LogManager.getLogger(StatsLoader.class);
-	
+
 	public static void main(String[] args) {
 		new StatsLoader().load();
 	}
@@ -24,93 +26,115 @@ public class StatsLoader extends HumanBeats
 	@Override
 	public void load(String... args)
 	{
+		int year = 2022;
+		int month = 2;
+		String source = "theblessedmadonna";
 		LOGGER.info("Calculating stats...");
-		TreeMap<Integer, Integer> scoreStats = Maps.newTreeMap();
-		//		TreeMap<String, TreeMap<String, Integer>> topCharts = Maps.newTreeMap();
-		MongoCursor<Document> i = docs.find(Filters.and(Filters.eq("type", "podcast"))).iterator();
-		int numPodcasts = 0;
-		int numTracks = 0;
-		int numMusicBrainz = 0;
-		int numMusicBrainzNA = 0;
+		LocalDateTime start = LocalDateTime.now().withYear(year).withMonth(month).with(TemporalAdjusters.firstDayOfMonth()).withHour(0).withMinute(0).withSecond(0);
+		LocalDateTime end = LocalDateTime.now().withYear(year).withMonth(month).with(TemporalAdjusters.lastDayOfMonth()).withHour(0).withMinute(0).withSecond(0);
+		MongoCursor<Document> i = docs.find(Filters.and(
+				Filters.eq("type", "podcast"),
+				Filters.eq("source", source), 
+				Filters.gt("date", start),
+				Filters.lt("date", end))).iterator();
+
+		TreeMap<Integer, List<String>> topArtists = Maps.newTreeMap();
+		TreeMap<String, Integer> artists = Maps.newTreeMap();
+		TreeMap<Integer, List<String>> topAlbums = Maps.newTreeMap();
+		TreeMap<String, Integer> albums = Maps.newTreeMap();
+		TreeMap<Integer, List<String>> topSongs = Maps.newTreeMap();
+		TreeMap<String, Integer> songs = Maps.newTreeMap();
+		TreeMap<Integer, List<String>> topVideos = Maps.newTreeMap();
+		TreeMap<String, Integer> videos = Maps.newTreeMap();
 		while(i.hasNext()) 
 		{
-			numPodcasts++;
 			Document page = i.next();
 			List<org.bson.Document> tracks = page.get("tracks", List.class);
-			//			String podcast = page.getString("artist");
-			//			Date date = page.getDate("date");
-			//			podcast = podcast+ "-" + new SimpleDateFormat("yyyy").format(date);
-			//			if(!topCharts.containsKey(podcast)) {
-			//				topCharts.put(podcast, Maps.newTreeMap());
-			//			}
-
 			if(CollectionUtils.isNotEmpty(tracks))
 			{
-				for(org.bson.Document track : tracks)
+				for(org.bson.Document t : tracks)
 				{
-					numTracks++;
-					Integer score = track.getInteger("score");
-					if(score == null) {
-						score = -1;
-					}
-					if(!scoreStats.containsKey(score)) {
-						scoreStats.put(score, 0);
-					}
-					scoreStats.put(score, scoreStats.get(score)+1);
-
-					//					String artistSp = track.getString("spartistid");
-					//					if(artistSp != null) {
-					//						TreeMap<String, Integer> topChart = topCharts.get(podcast);
-					//						if(!topChart.containsKey(artistSp)) {
-					//							topChart.put(artistSp, 0);
-					//						}
-					//						topChart.put(artistSp, topChart.get(artistSp)+1);
-					//					}
-					String artistMb = track.getString("artistid");
-					if(artistMb != null) {
-						if(!artistMb.equals(NA)) {
-							numMusicBrainz++;
+					String artist = t.getString("artist");
+					if(artist != null) {
+						Integer artistNum = artists.get(artist);
+						if(artistNum == null) {
+							artistNum = 0;
 						}
-						else {
-							numMusicBrainzNA++;
-						}
+						artists.put(artist, artistNum+1);
 					}
-				}
 
-				if(tracks.size() == 5) {
-					LOGGER.info(page.get("source") + ": " + page.get("id") + " (tracks " + tracks.size() + ")");
+					String album = t.getString("album");
+					if(album != null) {
+						Integer albumNum = albums.get(artist + " - " + album);
+						if(albumNum == null) {
+							albumNum = 0;
+						}
+						albums.put(artist + " - " + album, albumNum+1);
+					}
+					
+					String song = t.getString("spotify");
+					if(song != null) {
+						Integer songNum = songs.get(song);
+						if(songNum == null) {
+							songNum = 0;
+						}
+						songs.put(song, songNum+1);
+					}
+					
+					String video = t.getString("youtube");
+					if(video != null) {
+						Integer videoNum = videos.get(video);
+						if(videoNum == null) {
+							videoNum = 0;
+						}
+						videos.put(video, videoNum+1);
+					}
 				}
 			}
 		}
 
-		LOGGER.info("Podcasts Episodes: " + numPodcasts);
-		LOGGER.info("Podcasts Tracks: " + numTracks);
-		double perc = Double.valueOf(numMusicBrainz)/Double.valueOf(numTracks);
-		LOGGER.info("Podcasts MusicBrainz: " + numMusicBrainz + " (" + NumberFormat.getPercentInstance().format(perc) + ")");
-		perc = Double.valueOf(numMusicBrainzNA)/Double.valueOf(numTracks);
-		LOGGER.info("Podcasts MusicBrainz NA: " + numMusicBrainzNA + " (" + NumberFormat.getPercentInstance().format(perc) + ")");
-		for(Integer key : scoreStats.descendingKeySet()) {
-			int num = scoreStats.get(key);
-			perc = Double.valueOf(num)/Double.valueOf(numTracks);
-			LOGGER.info("Podcasts Tracks score " + key + ": " + num + " (" + NumberFormat.getPercentInstance().format(perc) + ")");
+		artists.keySet().forEach(a -> {
+			if(!topArtists.containsKey(artists.get(a))) {
+				topArtists.put(artists.get(a), Lists.newArrayList());
+			}
+			topArtists.get(artists.get(a)).add(a);
+		});
+		for(Integer rank : topArtists.descendingKeySet()) {
+			LOGGER.info("Artists: " + rank + " -> " + topArtists.get(rank));
 		}
-		//		LOGGER.info("--------------------");
-		//
-		//		LOGGER.info("Total Top Charts: " + topCharts.keySet().size());
-		//		for(String podcast : topCharts.descendingKeySet()) {
-		//			TreeMap<String, Integer> topChart = topCharts.get(podcast);
-		//			Map<String, Integer> sorted = topChart.entrySet().stream()
-		//					.sorted((Map.Entry.<String, Integer>comparingByValue().reversed()))
-		//					.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-		//			int j = 0;
-		//			for(String album : sorted.keySet()) {
-		//				LOGGER.info("Podcasts Top Charts " + podcast + ": " + album + " (" + sorted.get(album) + ")");
-		//				j++;
-		//				if(j== 10) {
-		//					break;
-		//				}
-		//			}
-		//			LOGGER.info("--------------------");
-		//		}
+		LOGGER.info("--------------------");
+		
+		albums.keySet().forEach(a -> {
+			if(!topAlbums.containsKey(albums.get(a))) {
+				topAlbums.put(albums.get(a), Lists.newArrayList());
+			}
+			topAlbums.get(albums.get(a)).add(a);
+		});
+		for(Integer rank : topAlbums.descendingKeySet()) {
+			LOGGER.info("Albums: " + rank + " -> " + topAlbums.get(rank));
+		}
+		LOGGER.info("--------------------");
+		
+		songs.keySet().forEach(s -> {
+			if(!topSongs.containsKey(songs.get(s))) {
+				topSongs.put(songs.get(s), Lists.newArrayList());
+			}
+			topSongs.get(songs.get(s)).add(s);
+		});
+		for(Integer rank : topSongs.descendingKeySet()) {
+			LOGGER.info("Songs: " + rank + " -> " + topSongs.get(rank));
+		}
+		LOGGER.info("--------------------");
+		
+		videos.keySet().forEach(a -> {
+			if(!topVideos.containsKey(videos.get(a))) {
+				topVideos.put(videos.get(a), Lists.newArrayList());
+			}
+			topVideos.get(videos.get(a)).add(a);
+		});
+		for(Integer rank : topVideos.descendingKeySet()) {
+			LOGGER.info("Videos: " + rank + " -> " + topVideos.get(rank));
+		}
+		LOGGER.info("--------------------");
 	}
 }

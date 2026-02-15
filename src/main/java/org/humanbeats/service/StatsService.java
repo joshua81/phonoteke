9623f4ea9e743c5ddb4e94fc5c35e2288adef47c
Andrea Ricci <andrea.ricci@prometeia.com>
2026-02-15 12:06:37 +0100

    Stats service cleanup

--- src/main/java/org/humanbeats/service/StatsService.java
+++ src/main/java/org/humanbeats/service/StatsService.java
@@ -6,10 +6,8 @@ import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.MapUtils;
@@ -21,7 +19,6 @@ import org.springframework.stereotype.Component;
 
 import com.google.api.client.util.Lists;
 import com.google.api.client.util.Maps;
-import com.google.api.client.util.Sets;
 import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.model.Filters;
@@ -35,21 +32,15 @@ public class StatsService
 	@Autowired
 	private MongoRepository repo;
 
-	private Map<String, Set<String>> affinity = Maps.newHashMap();
-
 	private Map<String, BigDecimal> artistsScore = Maps.newHashMap();
-	private Map<String, Document> artistsDocs = Maps.newHashMap();
 	private List<String> artistsList = Lists.newArrayList();
 
-	private Map<String, BigDecimal> albumsScore = Maps.newHashMap();
 	private Map<String, Document> albumsDocs = Maps.newHashMap();
 	private List<String> albumsList = Lists.newArrayList();
 
-	private Map<String, BigDecimal> songsScore = Maps.newHashMap();
 	private Map<String, Document> songsDocs = Maps.newHashMap();
 	private List<String> songsList = Lists.newArrayList();
 
-	private Map<String, BigDecimal> videosScore = Maps.newHashMap();
 	private Map<String, Document> videosDocs = Maps.newHashMap();
 	private List<String> videosList = Lists.newArrayList();
 
@@ -66,49 +57,12 @@ public class StatsService
 				source = page.getString("source");
 				calculateStats();
 			}
-			calculateAffinities();
 		}
 		else {
 			source = args[0];
 			year = args.length == 2 ? Integer.parseInt(args[1]) : null;
 			calculateStats();
 		}
-	}
-
-	private void calculateAffinities() {
-		affinity.keySet().forEach(source -> {
-			log.info("Calculating affinities " + source + "...");
-			Map<String, BigDecimal> affinities = calculateAffinitiesInternal();
-			BigDecimal affinitiesTot = affinities.values().stream().reduce(BigDecimal.ZERO, BigDecimal::add);
-
-			MongoCursor<Document> i = repo.getStats().find(Filters.and(Filters.eq("source", source))).iterator();
-			Document doc = i.next();
-			List<Document> affinitiesDoc = Lists.newArrayList();
-			affinities.keySet().forEach(artist -> {
-				affinitiesDoc.add(new Document("source", artist)
-						.append("affinity", affinities.get(artist).doubleValue()));
-			});
-			doc.append("affinities", affinitiesDoc);
-			doc.append("affinitiesTot", affinitiesTot.doubleValue());
-			repo.getStats().updateOne(Filters.eq("source", source), new org.bson.Document("$set", doc));
-			log.info(source + " updated");
-		});
-	}
-
-	private Map<String, BigDecimal> calculateAffinitiesInternal() {
-		Map<String, BigDecimal> affinities = Maps.newHashMap();
-		affinity.keySet().forEach(artist -> {
-			if(!source.equals(artist)) {
-				Set<String> artists1 = new HashSet<String>(affinity.get(source));
-				Set<String> artists2 = new HashSet<String>(affinity.get(artist));
-				int originalSize = artists1.size();
-				artists1.retainAll(artists2);
-				BigDecimal affinity = new BigDecimal(artists1.size()).divide(new BigDecimal(originalSize), 4, RoundingMode.HALF_UP);
-				log.debug("affinity(" + source + ", " + artist + "): " + affinity);
-				affinities.put(artist, affinity);
-			}
-		});
-		return affinities;
 	}
 
 	private void calculateStats() {
@@ -118,8 +72,6 @@ public class StatsService
 		MongoCursor<Document> i = repo.getStats().find(Filters.and(Filters.eq("source", source))).iterator();
 		Document doc = i.next();
 		if(year == null) {
-			doc.append("artists", null);//getArtists());
-			doc.append("artistsVar", null);//getArtistsVar().doubleValue());
 			doc.append("albums", getAlbums());
 			doc.append("albumsVar", getAlbumsVar().doubleValue());
 			doc.append("tracks", getTracks());
@@ -129,8 +81,6 @@ public class StatsService
 		}
 		else {
 			Document yearDoc = new Document();
-			//			yearDoc.append("artists", getArtists());
-			//			yearDoc.append("artistsVar", getArtistsVar().doubleValue());
 			yearDoc.append("albums", getAlbums());
 			yearDoc.append("albumsVar", getAlbumsVar().doubleValue());
 			yearDoc.append("tracks", getTracks());
@@ -145,18 +95,14 @@ public class StatsService
 
 	private void collectData() {
 		artistsScore = Maps.newHashMap();
-		artistsDocs = Maps.newHashMap();
 		artistsList = Lists.newArrayList();
 
-		albumsScore = Maps.newHashMap();
 		albumsDocs = Maps.newHashMap();
 		albumsList = Lists.newArrayList();
 
-		songsScore = Maps.newHashMap();
 		songsDocs = Maps.newHashMap();
 		songsList = Lists.newArrayList();
 
-		videosScore = Maps.newHashMap();
 		videosDocs = Maps.newHashMap();
 		videosList = Lists.newArrayList();
 
@@ -183,20 +129,12 @@ public class StatsService
 				if(artist != null && !HumanBeatsUtils.NA.equals(artist)) {
 					BigDecimal score = artistsScore.getOrDefault(artist, BigDecimal.ZERO);
 					artistsScore.put(artist, score.add(BigDecimal.ONE));
-					artistsDocs.put(artist, t);
 					artistsList.add(artist);
-
-					if(source != null) {
-						affinity.putIfAbsent(source, Sets.newHashSet());
-						affinity.get(source).add(artist);
-					}
 				}
 
 				// Album
 				String album = t.getString("spalbumid");
 				if(album != null && !HumanBeatsUtils.NA.equals(album)) {
-					BigDecimal score = albumsScore.getOrDefault(album, BigDecimal.ZERO);
-					albumsScore.put(album, score.add(BigDecimal.ONE));
 					albumsDocs.put(album, t);
 					albumsList.add(album);
 				}
@@ -204,31 +142,19 @@ public class StatsService
 				// Song
 				String song = t.getString("spotify");
 				if(song != null && !HumanBeatsUtils.NA.equals(song)) {
-					BigDecimal score = songsScore.getOrDefault(song, BigDecimal.ZERO);
-					songsScore.put(song, score.add(BigDecimal.ONE));
-					if(!songsDocs.containsKey(song)) {
-						songsDocs.put(song, t);
-					}
+					songsDocs.put(song, t);
 					songsList.add(song);
 				}
 
 				// Video
 				String video = t.getString("youtube");
 				if(video != null && !HumanBeatsUtils.NA.equals(video)) {
-					BigDecimal score = videosScore.getOrDefault(video, BigDecimal.ZERO);
-					videosScore.put(video, score.add(BigDecimal.ONE));
-					if(!videosDocs.containsKey(video)) {
-						videosDocs.put(video, t);
-					}
+					videosDocs.put(video, t);
 					videosList.add(video);
 				}
 			});
 		});
 	}
-
-	//	private BigDecimal getArtistsVar() {
-	//		return new BigDecimal(artistsDocs.keySet().size()).divide(new BigDecimal(artistsList.size()), 4, RoundingMode.HALF_UP);
-	//	}
 
 	private BigDecimal getAlbumsVar() {
 		return new BigDecimal(albumsDocs.keySet().size()).divide(new BigDecimal(albumsList.size()), 4, RoundingMode.HALF_UP);
@@ -240,7 +166,7 @@ public class StatsService
 
 	private List<Document> getVideos() {
 		List<Document> topVideos = Lists.newArrayList();
-		if(MapUtils.isNotEmpty(videosScore)) {
+		if(MapUtils.isNotEmpty(videosDocs)) {
 			List<Document> jsonVideos = Lists.newArrayList(videosDocs.values());
 			Collections.sort(jsonVideos, new Comparator<Document>() {
 				@Override
@@ -264,12 +190,11 @@ public class StatsService
 
 	private List<Document> getReviews() {
 		List<Document> topReviews = Lists.newArrayList();
-		if(MapUtils.isNotEmpty(artistsScore)) {
-			List<String> artists = Lists.newArrayList(artistsScore.keySet());
+		if(CollectionUtils.isNotEmpty(artistsList)) {
 			MongoCursor<Document> i = repo.getDocs().find(Filters.and(
 					Filters.eq("source", "ondarock"), 
 					Filters.eq("type", "album"), 
-					Filters.in("spartistid", artists)))
+					Filters.in("spartistid", artistsList)))
 					.sort(new BasicDBObject("year", -1).append("date", -1)).limit(100).iterator();
 			while(i.hasNext()) {
 				Document page = i.next();
@@ -321,24 +246,6 @@ public class StatsService
 		return topAlbums;
 	}
 
-	//	private List<Document> getArtists() {
-	//		List<Document> jsonArtists = Lists.newArrayList(artistsDocs.values());
-	//		Collections.sort(jsonArtists, new Comparator<Document>() {
-	//			@Override
-	//			public int compare(Document a1, Document a2) {
-	//				int res = artistScore(a2).compareTo(artistScore(a1));
-	//				return res != 0 ? res : documentDate(a2).compareTo(documentDate(a1));
-	//			}
-	//		});
-	//
-	//		List<Document> topArtists = Lists.newArrayList();
-	//		subList(jsonArtists).forEach(a -> {
-	//			log.debug("Artist: " + a.getString("artist") + ": " + artistScore(a));
-	//			topArtists.add(getArtist(a));
-	//		});
-	//		return topArtists;
-	//	}
-
 	private MongoCursor<Document> getDocs() {
 		// all episodes in the last month
 		if(source == null) {
@@ -370,11 +277,6 @@ public class StatsService
 					.sort(new BasicDBObject("date", -1)).iterator();
 		}
 	}
-
-	//	private Document getArtist(Document track) {
-	//		return new Document("artist", track.getString("artist"))
-	//				.append("spartistid", track.getString("spartistid"));
-	//	}
 
 	private Document getAlbum(Document track) {
 		return new Document("artist", track.getString("artist"))
@@ -409,11 +311,7 @@ public class StatsService
 		return new Document("id", review.getString("id"))
 				.append("artist", review.getString("artist"))
 				.append("album", review.getString("title"))
-				//.append("spartistid", review.getString("spartistid"))
-				//.append("spalbumid", review.getString("spalbumid"))
 				.append("cover", review.getString("coverM"))
-				//.append("description", review.getString("description"))
-				//.append("vote", review.getDouble("vote"))
 				.append("year", review.getInteger("year"));
 	}
 
@@ -428,18 +326,6 @@ public class StatsService
 	private BigDecimal artistScore(Document track) {
 		return artistsScore.getOrDefault(track.get("spartistid"), BigDecimal.ZERO);
 	}
-
-	//	private BigDecimal albumScore(Document track) {
-	//		return albumsScore.getOrDefault(track.get("spalbumid"), BigDecimal.ZERO);
-	//	}
-	//
-	//	private BigDecimal videoScore(Document track) {
-	//		return videosScore.getOrDefault(track.get("youtube"), BigDecimal.ZERO);
-	//	}
-	//
-	//	private BigDecimal trackScore(Document track) {
-	//		return songsScore.getOrDefault(track.get("spotify"), BigDecimal.ZERO);
-	//	}
 
 	private Date documentDate(Document doc) {
 		return doc.getDate("date");
